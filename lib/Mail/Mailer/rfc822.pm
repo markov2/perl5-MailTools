3558ab8d84419c954a83582f49f4bb0e72ab0e7c
Mark Overmeer <mark@overmeer.net>
2024-11-13 09:28:37 +0100

    To/Cc/Bcc/From fields may appear only once #4

--- lib/Mail/Mailer/rfc822.pm
+++ lib/Mail/Mailer/rfc822.pm
@@ -7,21 +7,22 @@ use base 'Mail::Mailer';
 
 use strict;
 
+# Some fields are not allowed to repeat
+my %max_once = map +($_ => 1), qw/from to cc bcc reply-to/;
+
 sub set_headers
 {   my ($self, $hdrs) = @_;
-
     local $\ = "";
 
-    foreach (keys %$hdrs)
-    {   next unless m/^[A-Z]/;
-
-        foreach my $h ($self->to_array($hdrs->{$_}))
-        {   $h =~ s/\n+\Z//;
-            print $self "$_: $h\n";
-        }
+    foreach my $f (grep /^[A-Z]/, keys %$hdrs)
+    {   # s///r requires perl 5.12: too new :-)
+        my @h = map { my $h = $_; $h =~ s/\n+\Z//; $h } $self->to_array($hdrs->{$f});
+        @h or next;
+        @h = join ', ', @h if $max_once{lc $f};
+        print $self "$f: $_\n" for @h;
     }
 
-    print $self "\n";	# terminate headers
+    print $self "\n";  # end of headers
 }
 
 1;
